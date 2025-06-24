f7066eaeb9ff130c4317e840fce30c7918a409e3
unknown <hafeez.arbaz08@gmail.com>
2025-06-24 20:17:07 +0530

    cors settings

--- cors_config.py
+++ cors_config.py
@@ -5,7 +5,7 @@ from fastapi.middleware.cors import CORSMiddleware
 def add_cors(app: FastAPI):
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=["http://localhost:5173"],  # <- exact React frontend port
+        allow_origins=["https://sleekwithdeals-ui.onrender.com"],  # <- exact React frontend port
         allow_methods=["*"],
         allow_headers=["*"],
     )
